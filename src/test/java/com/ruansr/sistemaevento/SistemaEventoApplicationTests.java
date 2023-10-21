50c3f96b73dee2a8ea9668ff37e81da3277df6f9
RuanSR <ruansilvarosa@gmail.com>
2023-10-21 20:46:18 -0300

    Projeto iniciado

---
+++ src/test/java/com/ruansr/sistemaevento/SistemaEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruansr.sistemaevento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
